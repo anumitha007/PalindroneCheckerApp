17f4572cb4ac90ea930ef32d8f4d9e17f355a5d4
anumitha <anumitha2020@gmail.com>
2026-02-27 09:41:01 +0530

    Anumitha Added UC2: Print a Hardcoded Palindrome Result

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,5 +1,16 @@
 public class PalindromeCheckerApp {
-    public static void main(String[] args){
-        System.out.println("welcome to palindrome checker app management system");
+    public static void main(String[] args) {
+        String word = "madam";
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a palindrome");
+        } else {
+            System.out.println(word + " is not a palindrome");
+        }
     }
 }
